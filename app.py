4100f021da38618d9a4f0283ecb1ca3c9418262c
priagungs <priagungsatyagama@gmail.com>
2018-12-12 12:34:27 +0700

    finished api for generating dummy data

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+import json
+import random
+import datetime
+
+app = Flask(__name__)
+with open('data.json') as file:
+    raw = file.read()
+    data = json.loads(raw)
+
+def generate_response(size):
+    raw = random.choices(data, k=size)
+    response = []
+    start_time = datetime.datetime.now()
+    for el in raw:
+        el["timestamp"] = start_time + datetime.timedelta(minutes=random.randrange(60))
+        el["high"] = random.randrange(0, 2000)
+        el["low"] = random.randrange(0, el["high"])
+        el["avg"] = round(random.uniform(el["low"], el["high"]), 2)
+        el["last"] = random.randrange(el["low"], el["high"])
+        el["value"] = round(random.uniform(10000, 1000000), 2)
+        response.append(el)
+    return sorted(response, key=lambda element: element['timestamp'], reverse=True)
+
+
+
+@app.route('/getstream', methods=['GET'])
+def stream_response():
+    return jsonify(generate_response(int(request.args.get('size'))))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=4992, debug=True)
