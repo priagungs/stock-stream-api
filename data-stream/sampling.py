25505dcc48b6a997ff41f5d546817709f6dd7147
Bethea Davida <betheazia17@gmail.com>
2018-12-15 12:03:47 +0700

    editing sampling codes

--- data-stream/sampling.py
+++ data-stream/sampling.py
@@ -7,18 +7,18 @@ def printSamples(samples):
         print(samples[i]) 
   
 def samplingStreams(stream, n, k): 
-        samples = [0]*k 
-        for i in range(k): 
-            samples[i] = stream[i]
-          
-        while(i < n): 
-            j = random.randrange(i+1)
-              
-            if(j < k): 
-                samples[j] = stream[i] 
-            i+=1
-           
-        return samples
+    samples = [0]*k 
+    for i in range(k): 
+        samples[i] = stream[i]
+        
+    while(i < n): 
+        j = random.randrange(i+1)
+            
+        if(j < k): 
+            samples[j] = stream[i] 
+        i+=1
+        
+    return samples
   
 if __name__ == "__main__": 
     with open('../data.json') as f:
